3855d78acb1c342b3a975b3ad363976b9238a966
smith494 <court14104@gmail.com>
2022-12-29 16:45:07 -0600

    Change to call example.org

--- src/com/courtneysmith/Main.java
+++ src/com/courtneysmith/Main.java
@@ -1,5 +1,45 @@
+package com.courtneysmith;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+         //  URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+         //  URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            URI baseUri = new URI("http://username:password@mynewserver.com:5000");
+            URI uri1 = new URI("/catalogue/phones?os=android#samsung"); // relative URI
+            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung"); // relative URI
+            URI uri3 = new URI("/stores/locations?zip=12345"); // relative URI
+            URI resolvedUri1 = baseUri.resolve(uri1);
+            URI resolvedUri2 = baseUri.resolve(uri2);
+            URI resolvedUri3 = baseUri.resolve(uri3);
+            URL url1 = resolvedUri1.toURL();
+            System.out.println("URL 1 = " + url1);
+            URL url2 = resolvedUri2.toURL();
+            System.out.println("URL 2 = " + url2);
+            URL url3 = resolvedUri3.toURL();
+            System.out.println("URL 3 = " + url3);
+
+            URI relativizedURI = baseUri.relativize(resolvedUri2);
+            System.out.println("Relativized URI = " + relativizedURI);
+          //  URI uri = new URI("hello");
+//            System.out.println("Scheme = " + uri.getScheme());
+//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+//            System.out.println("Authority = " + uri.getAuthority());
+//            System.out.println("User info = " + uri.getUserInfo());
+//            System.out.println("Host = " + uri.getHost());
+//            System.out.println("Port = " + uri.getPort());
+//            System.out.println("Path = " + uri.getPath());
+//            System.out.println("Query = " + uri.getQuery());
+//            System.out.println("Fragment = " + uri.getFragment());
+        }catch (URISyntaxException e){
+            System.out.println("URI Bad Syntax: " + e.getMessage());
+        }catch (MalformedURLException e){
+            System.out.println("URL Malformed: " + e.getMessage());
+        }
     }
 }
